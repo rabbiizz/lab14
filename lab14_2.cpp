c5959f00af189ca7830f6ae893985b1234057ce2
rabbiizz <Supakarn_k@cmu.ac.th>
2024-01-05 17:41:48 +0700

    lab14_2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,39 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[N][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+	//เซ็ตค่า B ทั้งหมดเป็น 0 
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			B[i][j] = false;
+		}
+	}
+	for(int i = 1; i < N-1; i++){
+		for(int j = 1; j < N-1; j++){
+			if(A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j]){
+				B[i][j] = true;
+			}else{
+				B[i][j] = false;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool B[N][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+	cout << B[i][j] << " ";
+}
+	cout << "\n";
+}
+}
